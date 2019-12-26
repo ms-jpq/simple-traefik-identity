c53a5d5df2a92c59864c3e1465a3e52754847554
Fly me to the moon <git@bigly.dog>
2019-12-26 01:28:38 -0800

    got headers list

--- src/controllers/entry.fs
+++ src/controllers/entry.fs
@@ -6,7 +6,9 @@ open STI.Views.Login
 open DomainAgnostic
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
-
+open System.IO
+open System.Collections.Generic
+open Microsoft.Extensions.Primitives
 
 [<Route("")>]
 type Entry(logger: ILogger<Entry>, deps: Container<Variables>) =
@@ -15,10 +17,22 @@ type Entry(logger: ILogger<Entry>, deps: Container<Variables>) =
     [<Route("")>]
     member self.Index() =
         async {
-            let ctx = self.HttpContext
-            echo ctx.Request.Headers
+            use stream = new StreamReader(self.HttpContext.Request.Body)
 
-            let html = Render deps.Boxed.background deps.Boxed.title ""
-            return self.Content(html, "text/html") :> ActionResult
+            let headers =
+                self.HttpContext.Request.Headers
+                |> Seq.cast<KeyValuePair<string, StringValues>>
+                |> Seq.map (fun x -> x.Key, x.Value)
+                |> Map.ofSeq
+            let! body = stream.ReadToEndAsync() |> Async.AwaitTask
+
+            headers
+            |> Map.toSeq
+            |> Seq.iter echo
+
+            echo "\n\n\n"
+            echo body
+
+            return 200
         }
         |> Async.StartAsTask
