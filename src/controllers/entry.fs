be11c0df395c09eb7183cebe0cc2e95092ca5591
Fly me to the moon <git@bigly.dog>
2019-12-26 02:03:10 -0800

    added auth layout

--- src/controllers/entry.fs
+++ src/controllers/entry.fs
@@ -14,25 +14,52 @@ open Microsoft.Extensions.Primitives
 type Entry(logger: ILogger<Entry>, deps: Container<Variables>) =
     inherit Controller()
 
-    [<Route("")>]
-    member self.Index() =
+    let checkAuth cookies domain path =
+        ()
+        true
+
+    let authorize domain path =
+        ()
+        ()
+
+    let fdAuth headers =
+        maybe {
+            let! domain = Map.tryFind "X-Forwarded-Host" headers
+            let! path = Map.tryFind "X-Forwarded-Uri" headers
+            return domain, path }
+
+
+    member private self.Read() =
         async {
-            use stream = new StreamReader(self.HttpContext.Request.Body)
+            let ctx = self.HttpContext.Request
+            use stream = new StreamReader(ctx.Body)
 
             let headers =
-                self.HttpContext.Request.Headers
+                ctx.Headers
                 |> Seq.cast<KeyValuePair<string, StringValues>>
                 |> Seq.map (fun x -> x.Key, x.Value)
                 |> Map.ofSeq
-            let! body = stream.ReadToEndAsync() |> Async.AwaitTask
 
-            headers
-            |> Map.toSeq
-            |> Seq.iter echo
+            let cookies =
+                ctx.Cookies
+                |> Seq.cast<KeyValuePair<string, string>>
+                |> Seq.map (fun x -> x.Key, x.Value)
+                |> Map.ofSeq
+
+            let! body = stream.ReadToEndAsync() |> Async.AwaitTask
+            return headers, cookies, body
+        }
+
+
+    [<Route("")>]
+    member self.Index() =
+        async {
+            let! headers, cookies, body = self.Read()
+            let domain, path = fdAuth headers |> Option.ForceUnwrap ""
+
 
             echo "\n\n\n"
-            echo body
 
-            return 200
+            return self.Content("", "text/html") :> ActionResult
         }
         |> Async.StartAsTask
