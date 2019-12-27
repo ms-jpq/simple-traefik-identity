315a63bfd614c6a500d3c3833242f6d2a6c8a75b
Fly me to the moon <git@bigly.dog>
2019-12-26 16:36:32 -0800

    almost there

--- src/controllers/entry.fs
+++ src/controllers/entry.fs
@@ -18,6 +18,40 @@ open System.Text
 open System.IdentityModel.Tokens.Jwt
 
 
+module Ingress =
+
+    type AuthState =
+        | Unauthenticated = 401
+        | Unauthorized = 403
+        | Authorized = 200
+
+    type JwtClaim =
+        { access: Domains }
+
+        static member Serialize claim =
+            match claim.access with
+            | All -> [ "access", "*" ] |> Map.ofSeq
+            | Named lst ->
+                let access = String.Join(",", lst |> Seq.toArray)
+                [ "access", access ] |> Map.ofSeq
+
+        static member DeSerialize claim =
+            maybe {
+                let! access = claim |> Map.tryFind "access"
+                let res =
+                    match access with
+                    | "*" -> { access = All }
+                    | c ->
+                        let a =
+                            c.Split(",")
+                            |> Seq.ofArray
+                            |> Named
+                        { access = a }
+                return res
+            }
+
+
+open Ingress
 
 [<Route("")>]
 type Entry(logger: ILogger<Entry>, deps: Container<Variables>, state: GlobalVar<State>) =
@@ -25,12 +59,6 @@ type Entry(logger: ILogger<Entry>, deps: Container<Variables>, state: GlobalVar<
 
     let cOpts = deps.Boxed.cookie
     let jOpts = deps.Boxed.jwt
-
-    let credentials =
-        let bytes = deps.Boxed.model.secret |> Encoding.UTF8.GetBytes
-        let key = SymmetricSecurityKey(bytes)
-        let algo = SecurityAlgorithms.HmacSha256Signature
-        SigningCredentials(key, algo)
 
 
     let cookiePolicy domain =
@@ -42,6 +70,22 @@ type Entry(logger: ILogger<Entry>, deps: Container<Variables>, state: GlobalVar<
         policy.Domain <- cOpts.domain |> Option.defaultValue domain
         policy
 
+    let credentials =
+        let bytes = deps.Boxed.model.secret |> Encoding.UTF8.GetBytes
+        let key = SymmetricSecurityKey(bytes)
+        let algo = SecurityAlgorithms.HmacSha256Signature
+        SigningCredentials(key, algo)
+
+
+    let validateJWT (token: string) =
+        let validation = TokenValidationParameters()
+        try
+            let principal = JwtSecurityTokenHandler().ValidateToken(token, validation, ref null)
+            principal.Claims
+            |> Seq.map (fun c -> c.Type, c.Value)
+            |> Map.ofSeq
+            |> Some
+        with _ -> None
 
     let newJTW claims =
         let now = DateTime.UtcNow
@@ -60,24 +104,38 @@ type Entry(logger: ILogger<Entry>, deps: Container<Variables>, state: GlobalVar<
 
         JwtSecurityTokenHandler().CreateEncodedJwt(desc)
 
+    let authorize domain path (resp: HttpResponse) =
+        let policy = cookiePolicy domain
 
-    let validateJWT (token: string) =
-        let validation = TokenValidationParameters()
-        try
-            let principal = JwtSecurityTokenHandler().ValidateToken(token, validation, ref null)
-            principal.Claims
-            |> Seq.map (fun c -> c.Type, c.Value)
-            |> Map.ofSeq
-            |> Some
-        with _ -> None
+        let token = newJTW Map.empty
+        resp.Cookies.Append(cOpts.name, token, policy)
+
+    let deauthorize domain path (resp: HttpResponse) =
+        let policy = cookiePolicy domain
+        resp.Cookies.Delete(cOpts.name, policy)
 
 
-    let checkAuth cookies =
-        let auth =
-            cookies
-            |> Map.tryFind cOpts.name
-            |> Option.bind validateJWT
-        auth
+    let checkAuth domain cookies =
+        maybe {
+            let! claims = cookies
+                          |> Map.tryFind cOpts.name
+                          |> Option.bind validateJWT
+            let! model = JwtClaim.DeSerialize claims
+            let auth =
+                match model.access with
+                | All -> AuthState.Authorized
+                | Named lst ->
+                    let chk =
+                        lst
+                        |> Set
+                        |> flip Set.contains
+                    match chk domain with
+                    | true -> AuthState.Authorized
+                    | false -> AuthState.Unauthorized
+            return auth
+        }
+        |> Option.defaultValue AuthState.Unauthenticated
+
 
     let fdAuth headers =
         let err = "Missing Required Headers :: X-Forwarded-Host, X-Forwarded-Uri"
@@ -93,25 +151,33 @@ type Entry(logger: ILogger<Entry>, deps: Container<Variables>, state: GlobalVar<
         |> Option.ForceUnwrap err
 
 
-    let authorize domain path (resp: HttpResponse) =
-        let policy = cookiePolicy domain
-
-        let token = newJTW Map.empty
-        resp.Cookies.Append(cOpts.name, token, policy)
-
-    let deauthorize domain path (resp: HttpResponse) =
-        let policy = cookiePolicy domain
-        resp.Cookies.Delete(cOpts.name, policy)
-
-
-    [<Route("")>]
+    [<HttpGet("")>]
     member self.Index() =
         async {
-            let headers, cookies = Exts.metadata self.HttpContext.Request
+            let req = self.HttpContext.Request
+            let resp = self.HttpContext.Response
+            let headers, cookies = Exts.metadata req
             let domain, path = fdAuth headers
-            let authStat = checkAuth cookies
+            let auth = checkAuth domain cookies
 
 
+            resp.StatusCode <- LanguagePrimitives.EnumToValue auth
+            return self.Content("", "text/html") :> ActionResult
+        }
+        |> Async.StartAsTask
+
+
+    [<HttpPost("")>]
+    member self.Auth(username: string, password: string) =
+        async {
+            let req = self.HttpContext.Request
+            let resp = self.HttpContext.Response
+            let headers, cookies = Exts.metadata req
+            let domain, path = fdAuth headers
+            let auth = checkAuth domain cookies
+
+
+            resp.StatusCode <- LanguagePrimitives.EnumToValue auth
             return self.Content("", "text/html") :> ActionResult
         }
         |> Async.StartAsTask
