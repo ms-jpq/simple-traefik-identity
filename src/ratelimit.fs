1c20fe630e77d15cc76d3bcba5294f1b4530e007
Fly me to the moon <git@bigly.dog>
2019-12-31 00:03:42 -0800

    added auth

--- src/ratelimit.fs
+++ src/ratelimit.fs
@@ -1,5 +1,6 @@
 namespace STI
 
+open DomainAgnostic
 open STI.Env
 open STI.State
 open System
@@ -10,3 +11,28 @@ module RateLimit =
     type Limit =
         | Umlimited
         | Limited of TimeSpan
+
+
+    let auth (limit: RateLimit) (state: State) ip (now: DateTime) =
+        let ago = now - limit.timer
+        state.history
+        |> Map.tryFind ip
+        |> Option.defaultValue Seq.empty
+        |> Seq.Count((<=) ago)
+        |> (>=) limit.rate
+
+    let update (state: State) ip now =
+        let hist =
+            state.history
+            |> Map.tryFind ip
+            |> Option.defaultValue Seq.empty
+            |> Seq.Appending now
+            |> flip (Map.add ip) state.history
+        { history = hist }
+
+
+    let next limit state ip =
+        let now = DateTime.UtcNow
+        let res = auth limit state ip now
+        let next = update state ip now
+        res, next
