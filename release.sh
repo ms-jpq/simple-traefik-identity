f2cda3d5728329ccdf57d510c176a7839b06bc7a
Fly me to the moon <git@bigly.dog>
2020-02-18 13:14:09 -0800

    fix shellcheck

--- release.sh
+++ release.sh
@@ -5,6 +5,6 @@ set -o pipefail
 
 # docker-compose up -d --build
 
-docker tag $1 msjpq/simple-traefik-identity:latest
+docker tag "$1" msjpq/simple-traefik-identity:latest
 
 docker push msjpq/simple-traefik-identity:latest
